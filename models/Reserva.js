138a7bdf5e9b73b2ca3787c62bd50a80af7eef84
PiazzaCM <carlamalenapiazza@gmail.com>
2023-07-05 15:09:05 -0300

    modelo para reservas

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,1 +1,35 @@
 // TODO: Crear modelo de datos de Reserva
+const { sequelize, Model, DataTypes} = require('sequelize')
+
+const Reserva = sequelize.define('reserva', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    nombre: {
+        type: DataTypes.STRING,
+        require: true
+    },
+    apellido: {
+        type: DataTypes.STRING,
+        require: true
+    },
+    fechaSolicitud: {
+        type: DataTypes.STRING,
+        require: true,
+        default: new Date().toLocaleDateString('es', { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' })
+    },
+    fechaSolicitada: {
+        type: DataTypes.STRING,
+        require: true,
+    },
+    codigoReserva: {
+        type: DataTypes.INTEGER,
+        require: true
+    }
+});
+
+Reserva.sync()
+
+module.exports = Reserva
