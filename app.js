27b36eaad1b18584c9540972cd23fb23c05391c9
PiazzaCM <carlamalenapiazza@gmail.com>
2023-07-05 14:55:09 -0300

    se implemento los middlewares necesarios, se configuro las rutas y el puerto, tambien se instalo las dependencias necesarias

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 // Imports
 const cors = require('cors');
 const express = require('express');
-
+const morgan = require('morgan');
+require('dotenv').config()
 const path = require('path');
-
+const helmet = require('helmet');
 const app = express();
 
 // Middlewares
 // TODO: Implementar middlewares
-
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-app.use('/api', require('./routes/reserva.routes'));
+app.use(require('./routes/reserva.routes'));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
+app.use((req, res, next) => {
+    return res.status(404).render('404');
+    
+})
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(process.env.PORT, () => console.log(`Server on port ${process.env.PORT}`));
